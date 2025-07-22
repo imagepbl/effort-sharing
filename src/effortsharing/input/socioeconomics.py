7786f8503ef8d124c6882422509041dc38d4835e
sverhoeven <s.verhoeven@esciencecenter.nl>
2025-07-22 11:07:41 +0200

    Fix: ValueError: unable to infer dtype on variable 'Region'; object array contains mixed native types: str_, str

--- src/effortsharing/input/socioeconomics.py
+++ src/effortsharing/input/socioeconomics.py
@@ -183,6 +183,7 @@ def read_ssps(config, regions, countries):
                 .reindex({"Time": np.arange(1980, 2101, 5)})
             )
 
+    xr_ssp['Region'] = xr_ssp.Region.astype(str)
     return xr_ssp
 
 
